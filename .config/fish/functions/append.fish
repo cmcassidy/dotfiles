e539543a6abecbc264dc81110a809c1ed2dfea42
Connor Cassidy <connormcassidy@gmail.com>
2016-06-30 14:17:16 -0400

    screwed up bad with links, here is the real stuff

--- .config/fish/functions/append.fish
+++
@@ -1,8 +0,0 @@
-function append --no-scope-shadowing
-	if test (count $argv) -ne 2
-        echo append: Expected two arguments, (count $argv) received.
-        return 1
-    end
-    set -l __fish_value $$argv[1]
-    set $argv[1] "$__fish_value$argv[2]"
-end
